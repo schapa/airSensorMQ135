2da65fc15f660851cdebe5471f930f332feddb64
Pavlo Hamov <pavlo.hamov@dev.zodiac.tv>
2016-01-21 18:21:46 +0200

    Add ncircle buffer

---
+++ include/circleBuffer.h
@@ -0,0 +1,33 @@
+/*
+ * circleBuffer.h
+ *
+ *  Created on: Jan 21, 2016
+ *      Author: shapa
+ */
+
+#ifndef CIRCLEBUFFER_H_
+#define CIRCLEBUFFER_H_
+
+#include <stdint.h>
+
+typedef struct {
+	uint32_t *data;
+	uint32_t size;
+
+	uint32_t firstIdx;
+	uint32_t curIdx;
+	uint32_t occupied;
+
+} CircleBuffer_t, *CircleBuffer_p;
+
+CircleBuffer_p CircleBuffer_new(uint32_t size);
+void CircleBuffer_delete(CircleBuffer_p buff);
+
+uint32_t CircleBuffer_size(CircleBuffer_p buff);
+uint32_t CircleBuffer_getOccupiedSize(CircleBuffer_p buff);
+uint32_t CircleBuffer_getAt(CircleBuffer_p buff, uint32_t idx);
+void CircleBuffer_pushEnd(CircleBuffer_p buff, uint32_t data);
+
+
+
+#endif /* CIRCLEBUFFER_H_ */
---
+++ src/circleBuffer.c
@@ -0,0 +1,96 @@
+/*
+ * circleBuffer.c
+ *
+ *  Created on: Jan 21, 2016
+ *      Author: shapa
+ */
+
+
+#include "circleBuffer.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+
+CircleBuffer_p CircleBuffer_new(uint32_t size) {
+	CircleBuffer_p newBuff = NULL;
+
+	do {
+		newBuff = (CircleBuffer_p)malloc(sizeof(CircleBuffer_t));
+		if (!newBuff)
+			return NULL;
+		newBuff->data = (uint32_t*)malloc(size*sizeof(*newBuff->data));
+		if (!newBuff->data) {
+			CircleBuffer_delete(newBuff);
+			return NULL;
+		}
+		newBuff->size = size;
+		newBuff->firstIdx = 0;
+		newBuff->curIdx = 0;
+		newBuff->occupied = 0;
+
+	} while (0);
+
+	return newBuff;
+}
+void CircleBuffer_delete(CircleBuffer_p buff) {
+	if (buff && buff->data)
+		free(buff->data);
+	if (buff)
+		free(buff);
+}
+
+uint32_t CircleBuffer_size(CircleBuffer_p buff) {
+	uint32_t size = 0;
+	if (buff && buff->data) {
+		size = buff->size;
+	}
+	return size;
+}
+
+uint32_t CircleBuffer_getOccupiedSize(CircleBuffer_p buff) {
+	uint32_t size = 0;
+	if (buff && buff->data) {
+		size = buff->occupied;
+	}
+	return size;
+}
+
+uint32_t CircleBuffer_getAt(CircleBuffer_p buff, uint32_t idx) {
+	uint32_t value = 0;
+	uint32_t maxIdx = 0;
+
+	do {
+		if (!buff || idx >= buff->occupied) {
+			break;
+		}
+		maxIdx = buff->occupied - 1;
+		idx += buff->firstIdx;
+		if (idx > maxIdx) {
+			idx -= buff->occupied;
+		}
+		value = buff->data[idx];
+	} while (0);
+
+	return value;
+}
+
+void CircleBuffer_pushEnd(CircleBuffer_p buff, uint32_t data) {
+
+	if (!buff || !buff->size) {
+		return;
+	}
+
+	buff->data[buff->curIdx++] = data;
+
+	if (buff->curIdx >= buff->size) {
+		buff->curIdx = 0;
+	}
+
+	if (buff->occupied < buff->size) {
+		buff->occupied++;
+	} else {
+		if (++buff->firstIdx >= buff->size) {
+			buff->firstIdx = 0;
+		}
+	}
+}
